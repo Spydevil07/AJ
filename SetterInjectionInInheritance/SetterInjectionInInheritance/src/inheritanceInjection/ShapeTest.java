0ca87dfb719938af3a125d1b2dd152ed3fbce83a
Spydevil07 <157968450+Spydevil07@users.noreply.github.com>
2024-01-30 00:13:12 +0530

    Add files via upload

---
+++ SetterInjectionInInheritance/SetterInjectionInInheritance/src/inheritanceInjection/ShapeTest.java
@@ -0,0 +1,17 @@
+package inheritanceInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ShapeTest {
+	private static ApplicationContext appCon;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		appCon = new ClassPathXmlApplicationContext("appctx.xml"); 
+		ShapeManager factory=(ShapeManager)appCon.getBean("shapeBean1"); 
+		ShapeManager factory2=(ShapeManager)appCon.getBean("shapeBean2"); 
+		factory.show();
+		factory2.show();
+	}
+}
---
+++ SetterInjectionInInheritance/SetterInjectionInInheritance/src/inheritanceInjection/rectangle.java
@@ -0,0 +1,9 @@
+package inheritanceInjection;
+
+public class rectangle implements Shape {
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.println("I am rec...");
+	}
+}
